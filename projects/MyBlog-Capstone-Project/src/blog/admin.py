e841a4ac023e509be373ea55f7f39e16e6f324cd
yasint23 <pakyasin23@gmail.com>
2022-03-29 22:55:54 +0100

    AWS Certification docs created

--- projects/MyBlog-Capstone-Project/src/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post, Like, PostView, Comment
-
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(PostView)
-admin.site.register(Comment)
-
-# Register your models here.
--- projects/aws_capstone_project/src/blog/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogConfig(AppConfig):
-    name = 'blog'
-
-    def ready(self):
-        import blog.signals
